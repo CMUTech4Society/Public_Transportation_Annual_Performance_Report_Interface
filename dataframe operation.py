e308fd833538a65ae53d596ca0a73f9a0ba9b740
Yisu Ding <yisud@andrew.cmu.edu>
2025-10-27 18:51:08 -0400

    Add files via upload

--- dataframe operation.py
+++ dataframe operation.py
@@ -1,2 +1,12 @@
 import pandas as pd
 from typing import Dict, Tuple, Union, List
+def data_underscore_query(
+        X: pd.DataFrame,
+        Y: pd.DataFrame,
+        filters: pd.DataFrame,
+        merge_key: str,
+        conditions: Dict[str, Union[Tuple[str, float], float]],
+        multiplier_col: str = None,
+        multiplier: float = 1.0
+)-> Tuple[pd.DataFrame, pd.DataFrame]:
+    
