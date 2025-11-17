61b4e6c1954aeb278a8bbc9fd7f47f94367e9c9d
Yiyoung <yiyoung.md@gmail.com>
2025-11-17 17:46:34 -0500

    fix because i forgot

--- src/graph.py
+++ src/graph.py
@@ -5,12 +5,12 @@ def graph(PlotStruct):
     df = pd.merge(PlotStruct.x_axis.dataframe, PlotStruct.y_axis.dataframe, on=['department', 'year'], suffixes=('_x', '_y'))
     fig = px.scatter(
         df,
-        x=PlotStruct.x_axis.label,
-        y=PlotStruct.y_axis.label,
+        x=f'{PlotStruct.x_axis.label}_x',
+        y=f'{PlotStruct.y_axis.label}_y',
         color='department',
         title=f'{PlotStruct.y_axis.label} vs {PlotStruct.x_axis.label} by Department',
         labels={
-            PlotStruct.x_axis.label: PlotStruct.x_axis.label,
-            PlotStruct.y_axis.label: PlotStruct.y_axis.label}
+            f'{PlotStruct.x_axis.label}_x': PlotStruct.x_axis.label,
+            f'{PlotStruct.y_axis.label}_y': PlotStruct.y_axis.label}
     )
     fig.show()
