4dabd210dc47cac82aa5007d12dfa0965d88fa79
garrison <garrisoc@andrew.cmu.edu>
2025-11-17 17:20:20 -0500

    added global objects

---
+++ src/global.py
@@ -0,0 +1,13 @@
+class PlotStruct:
+    def __init__(self, meta, x_axis, y_axis, x_filter, y_filter, multiplier):
+        self.meta = meta                # None
+        self.x_axis = x_axis            # dataset
+        self.y_axis = y_axis            # dataset
+        self.x_filter = x_filter        # None/[dataset]
+        self.y_filter = y_filter        # None/[dataset]
+        self.multiplier = multiplier    # None/[dataset]
+
+class dataset:
+    def __init__(self, label, func):
+        self.label = label
+        self.func = func 
