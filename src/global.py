e6ade8fe5d458466bbf6a386ddc938f6fbd8c161
garrison <garrisoc@andrew.cmu.edu>
2025-11-17 17:38:22 -0500

    added filter class to make filters/datasets distinct

--- src/global.py
+++ src/global.py
@@ -3,11 +3,16 @@ class PlotStruct:
         self.meta = meta                # None
         self.x_axis = x_axis            # dataset
         self.y_axis = y_axis            # dataset
-        self.x_filter = x_filter        # None/[dataset]
-        self.y_filter = y_filter        # None/[dataset]
-        self.multiplier = multiplier    # None/[dataset]
+        self.x_filter = x_filter        # None/[filter]
+        self.y_filter = y_filter        # None/[filter]
+        self.multiplier = multiplier    # None/[filter]
 
 class dataset:
+    def __init__(self, label, dataframe):
+        self.label = label
+        self.dataframe = dataframe
+
+class filter:
     def __init__(self, label, func):
         self.label = label
         self.func = func 
