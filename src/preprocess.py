11b47ccc1d9c26350136192ae1c8a4c99e7a6abc
garrison <garrisoc@andrew.cmu.edu>
2025-11-17 17:47:44 -0500

    implemented x_axis, y_axis, and filters for preprocess

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,6 +1,13 @@
+import shutil
+import shlex
 from fuzzywuzzy import fuzz
 from types import SimpleNamespace
 import fetch_database as fetch_database
+
+class dataset:
+    def __init__(self, label, func):
+        self.label = label
+        self.func = func 
 
 def get_closest_match(table_names, search_term):
     best_match = None
@@ -12,11 +19,56 @@ def get_closest_match(table_names, search_term):
             best_match = table_name
     return best_match
 
-def preprocess(conn, data):
+def preprocess(args):
+    conn = DatabaseConnection();
     # x_axis, y_axis, [x_filter], [y_filter], multiplier
     table_names = conn.get_table_names()
-    x_axis_dataset = conn.fetch_data(get_closest_match(table_names, data.x_axis))
-    y_axis_dataset = conn.fetch_data(get_closest_match(table_names, data.y_axis))
-    x_filter_datasets = [conn.fetch_data(get_closest_match(table_names, filter_name)) for filter_name in data.x_filters]
-    y_filter_datasets = [conn.fetch_data(get_closest_match(table_names, filter_name)) for filter_name in data.y_filters]
-    multiplier_dataset = conn.fetch_data(get_closest_match(table_names, data.multiplier))
+    x_dataframe = conn.fetch_data(get_closest_match(table_names, args.x_axis))
+    y_dataframe = conn.fetch_data(get_closest_match(table_names, args.y_axis))
+
+    x_dataset = dataset(args.x_axis, x_axis_dataframe)
+    y_dataset = dataset(args.y_axis, y_axis_dataframe)
+    
+    x_filters = []
+    for x in args.x_filters:
+        func = get_filter(x)
+        x_filters.append(Filter(x, func))
+
+    y_filters = []
+    for y in args.y_filters:
+        func = get_filter(y)
+        y_filters.append(Filter(y, func))
+
+
+    return PlotStruct(None, x_dataset, y_dataset, x_filters, y_filters, None)
+
+def get_filter_func(f): # f="population > 500"
+    if f is None: return None
+    column, operator, value = shlex.split(f)
+    table_names = conn.get_table_names()
+    column = get_closest_match(table_names, column)
+
+    match operator:
+        case ">":
+            operator = lambda a, b: a > b
+            break
+        case ">=":
+            operator = lambda a, b: a >= b
+            break
+        case "<":
+            operator = lambda a, b: a < b
+            break
+         case "<=":
+            operator = lambda a, b: a <= b
+            break
+         case "=":
+            operator = lambda a, b: a == b
+            break
+         case "!=":
+            operator = lambda a, b: a != b
+            break
+
+    if value.replace('.', ' ', 1).isnumeric():
+        return lambda row: operator(row.loc[column], float(value))
+    else
+        return lambda row: operator(row.loc[column], value)
